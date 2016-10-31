7fa26a8caa6cad1cadb98f1f1cc93bd4b3860f5c
quoc-cuong <quoc-cuong@it-developers.com>
2016-10-31 14:00:24 +0700

    add template angularjs & api

---
+++ ng.Net1/ng.Net1/Assets/app/controllers/testCtrl.js
@@ -0,0 +1,6 @@
+﻿angular.module('test', [])
+    .controller('testCtrl',['$scope','$http', function ($scope, $http) {
+        $scope.alert = function () {
+            alert("WOW");
+        }
+    }]);
